9acfc945345e19188ff03a7b3be02b3f602931a5
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-10-13 11:10:53 +0700

    🌱 update Name image

--- lib/core/configs/assets/app_vectors.dart
+++ lib/core/configs/assets/app_vectors.dart
@@ -3,8 +3,9 @@ class AppVectors {
   static const String format = '.svg';
 
   static const String logo = '${basePath}spotify_logo$format';
-  static const String sun = '${basePath}Sun$format';
-  static const String moon = '${basePath}Moon$format';
-  static const String unionTop = '${basePath}UnionTop$format';
-  static const String unionBottom = '${basePath}UnionBottom$format';
+  static const String sun = '${basePath}sun$format';
+  static const String moon = '${basePath}moon$format';
+  static const String unionTop = '${basePath}union_top$format';
+  static const String unionBottom = '${basePath}union_bottom$format';
+  static const String unionHomeArtistTop = '${basePath}union_home_top$format';
 }
