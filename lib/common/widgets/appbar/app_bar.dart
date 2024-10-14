9737b7954aecdb53ace4d719d6d0ea9a3783aacd
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-10-14 09:30:22 +0700

    ⬇️ add hiveback in Basic app bar

--- lib/common/widgets/appbar/app_bar.dart
+++ lib/common/widgets/appbar/app_bar.dart
@@ -4,9 +4,11 @@ import 'package:spotify_with_flutter/core/configs/theme/app_color.dart';
 
 class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
   final Widget title;
+  final bool hiveBack;
 
   const BasicAppBar({
     super.key,
+    this.hiveBack = false,
     this.title = const Text(''),
   });
 
@@ -17,26 +19,28 @@ class BasicAppBar extends StatelessWidget implements PreferredSizeWidget {
       centerTitle: true,
       backgroundColor: Colors.transparent,
       elevation: 0,
-      leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
-        },
-        icon: Container(
-          height: 50,
-          width: 50,
-          decoration: BoxDecoration(
-            color: context.isDarkMode
-                ? AppColors.white.withOpacity(0.03)
-                : AppColors.dark.withOpacity(0.04),
-            shape: BoxShape.circle,
-          ),
-          child: Icon(
-            Icons.arrow_back_ios_new,
-            size: 20,
-            color: context.isDarkMode ? AppColors.white : AppColors.dark,
-          ),
-        ),
-      ),
+      leading: hiveBack
+          ? null
+          : IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: context.isDarkMode
+                      ? AppColors.white.withOpacity(0.03)
+                      : AppColors.dark.withOpacity(0.04),
+                  shape: BoxShape.circle,
+                ),
+                child: Icon(
+                  Icons.arrow_back_ios_new,
+                  size: 20,
+                  color: context.isDarkMode ? AppColors.white : AppColors.dark,
+                ),
+              ),
+            ),
     );
   }
 
